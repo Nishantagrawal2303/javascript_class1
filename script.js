8fc6a8f272600049f3ebd12edfada079e3b1ba7a
Nishant Agrawal <nishant2303agarwal@gmail.com>
2025-08-19 22:55:36 +0530

    js

---
+++ script.js
@@ -0,0 +1,53 @@
+
+
+// function person(x,y){
+//     console.log("a,b",x,y);
+
+//     return a+b;
+
+// }
+
+// person(2,5);
+// console.log("x")
+
+// function hello(t,d){
+//     console.log("x,y", t,d)
+
+//        z=t+d;
+//         p=10+5;
+//      return z,p;
+    
+// }
+
+// let x=hello(10,20);
+// console.log(z);
+// console.log(p);
+
+
+
+// function add(){
+//     console.log("hrr");
+// }
+
+// let x=add()
+// console.log(x);
+
+
+// console.log("normal function");
+
+// sayGreet;
+
+// console.log(sayGreet);
+// function sayGreet(){
+//     console.log("good moring");
+    
+// }
+
+// sayGreet();
+
+
+
+function add(a,b){
+  return a+b;
+}
+console.log(add(2,3));
